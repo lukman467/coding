63b31d35fd043cde05356856a8e5d336dae938d2
lukman467 <148670387+lukman467@users.noreply.github.com>
2024-07-06 23:27:59 +0700

    Add files via upload

---
+++ kalkulator 2/script.js
@@ -0,0 +1,25 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+    
+    // This will replace the result of any mathematical operation with "I love you"
+    if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
+        display.value = 'I love you';
+    } else {
+        try {
+            display.value = eval(expression);
+        } catch {
+            display.value = 'Error';
+        }
+    }
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
