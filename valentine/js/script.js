3ff75d79632d8da18023950ad51a0cc9cc40aa15
lukman juventino <148670387+lukman467@users.noreply.github.com>
2025-02-14 20:36:46 +0700

    Add files via upload

---
+++ valentine/js/script.js
@@ -0,0 +1,8 @@
+const noBtn = document.getElementById('noBtn');
+
+noBtn.addEventListener('mouseover', () => {
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 50);
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
